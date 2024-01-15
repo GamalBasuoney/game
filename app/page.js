4c89bad86bb504a620b631bcfb40f8cb92b10145
GamalBasuoney <gamalbasuoneygb.gb@gmail.com>
2024-01-15 22:53:45 +0200

    icons

--- app/page.js
+++ app/page.js
@@ -1,8 +1,41 @@
 export default function Home() {
   return (
-    // here html masry
-    <main className="home">
-      hello Gamelbasuoney
-    </main>
+    <html lang="en">
+      <head>
+        <title>Gaming</title>
+      </head>
+
+      <body>
+        <nav>
+
+          <div>
+            <input type="search" placeholder="Search Product" />
+            <h2>Gaming<span>.</span></h2>
+            <ul class="icons">
+              <li> <img src="../public/huser.svg" />
+              </li>
+              <li>  <img src="../public/hwishlist.svg" />
+              </li>
+              <li>  <img src="../public/hcart.svg.svg" />
+              </li>
+            </ul>
+          </div>
+
+          <div>
+            <span>New Accessories -30% Off. More</span>
+            <ul>
+              <li>Warcraft</li>
+              <li>Gaming PCs</li>
+              <li>Online Games</li>
+              <li>Gaming Accessories</li>
+              <li>Gaming Chairs</li>
+              <li>Gaming Consoles</li>
+            </ul>
+          </div>
+        </nav>
+
+      </body>
+    </html>
+
   )
 }
